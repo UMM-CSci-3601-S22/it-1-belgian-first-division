fdbc6c088d2ab4afe364ca15516300ed19299d63
Zeke <krug0102@morris.umn.edu>
2022-03-01 16:02:50 -0600

    Add tests to get all products and filter by name
    Co-authored-by: kahnurd <hetti031@morris.umn.edu>
    Co-authored-by: nikfbail <bail0301@morris.umn.edu>

--- server/src/test/java/umm3601/product/ProductControllerSpec.java
+++ server/src/test/java/umm3601/product/ProductControllerSpec.java
@@ -44,6 +44,190 @@ import io.javalin.http.NotFoundResponse;
 import io.javalin.http.util.ContextUtil;
 import io.javalin.plugin.json.JavalinJackson;
 
+@SuppressWarnings({ "MagicNumber" })
 public class ProductControllerSpec {
+
+  private MockHttpServletRequest mockReq = new MockHttpServletRequest();
+  private MockHttpServletResponse mockRes = new MockHttpServletResponse();
+
+  // An instance of the controller we're testing that is prepared in
+  // `setupEach()`, and then exercised in the various tests below.
+  private ProductController productController;
+
+  // The client and database that will be used
+  // for all the tests in this spec file.
+  private static MongoClient mongoClient;
+  private static MongoDatabase db;
+
+  // Used to translate between JSON and POJOs.
+  private static JavalinJackson javalinJackson = new JavalinJackson();
+
+  /**
+   * Sets up (the connection to the) DB once; that connection and DB will
+   * then be (re)used for all the tests, and closed in the `teardown()`
+   * method. It's somewhat expensive to establish a connection to the
+   * database, and there are usually limits to how many connections
+   * a database will support at once. Limiting ourselves to a single
+   * connection that will be shared across all the tests in this spec
+   * file helps both speed things up and reduce the load on the DB
+   * engine.
+   */
+  @BeforeAll
+  public static void setupAll() {
+    String mongoAddr = System.getenv().getOrDefault("MONGO_ADDR", "localhost");
+
+    mongoClient = MongoClients.create(
+        MongoClientSettings.builder()
+            .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(mongoAddr))))
+            .build()
+    );
+    db = mongoClient.getDatabase("test");
+  }
+
+  @AfterAll
+  public static void teardown() {
+    db.drop();
+    mongoClient.close();
+  }
+
+  @BeforeEach
+  public void setupEach() throws IOException {
+    // Reset our mock request and response objects
+    mockReq.resetAll();
+    mockRes.resetAll();
+
+    // Setup database
+    MongoCollection<Document> productDocuments = db.getCollection("products");
+    productDocuments.drop();
+    List<Document> testProducts = new ArrayList<>();
+    testProducts.add(
+        new Document()
+            .append("name", "Apple")
+            .append("quantity", 25)
+            .append("brand", "UMM")
+            .append("comment", "These are good apples."));
+    testProducts.add(
+        new Document()
+            .append("name", "Grape")
+            .append("quantity", 1)
+            .append("brand", "")
+            .append("comment", "Grapes are tasty"));
+    testProducts.add(
+        new Document()
+            .append("name", "Potato")
+            .append("quantity", 2)
+            .append("brand", "Conner's Potatoes")
+            .append("comment", "We love Conner's Potatoes!"));
+
+    productDocuments.insertMany(testProducts);
+  }
+
+  /**
+   * Construct an instance of `Context` using `ContextUtil`, providing
+   * a mock context in Javalin. See `mockContext(String, Map)` for
+   * more details.
+   */
+  private Context mockContext(String path) {
+    return mockContext(path, Collections.emptyMap());
+  }
+
+  /**
+   * Construct an instance of `Context` using `ContextUtil`, providing a mock
+   * context in Javalin. We need to provide a couple of attributes, which is
+   * the fifth argument, which forces us to also provide the (default) value
+   * for the fourth argument. There are two attributes we need to provide:
+   *
+   *   - One is a `JsonMapper` that is used to translate between POJOs and JSON
+   *     objects. This is needed by almost every test.
+   *   - The other is `maxRequestSize`, which is needed for all the ADD requests,
+   *     since `ContextUtil` checks to make sure that the request isn't "too big".
+   *     Those tests fails if you don't provide a value for `maxRequestSize` for
+   *     it to use in those comparisons.
+   */
+  private Context mockContext(String path, Map<String, String> pathParams) {
+    return ContextUtil.init(
+        mockReq, mockRes,
+        path,
+        pathParams,
+        HandlerType.INVALID,
+        Map.ofEntries(
+          entry(JSON_MAPPER_KEY, javalinJackson),
+          entry(ContextUtil.maxRequestSizeKey,
+                new JavalinConfig().maxRequestSize
+          )
+        )
+      );
+  }
+
+  /**
+   * A little helper method that assumes that the given context
+   * body contains an array of Products, and extracts and returns
+   * that array.
+   *
+   * @param ctx the `Context` whose body is assumed to contain
+   *  an array of `Product`s.
+   * @return the array of `Product`s from the given `Context`.
+   */
+  private Product[] returnedProducts(Context ctx) {
+    String result = ctx.resultString();
+    Product[] products = javalinJackson.fromJsonString(result, Product[].class);
+    return products;
+  }
+
+  /**
+   * A little helper method that assumes that the given context
+   * body contains a *single* Product, and extracts and returns
+   * that Product.
+   *
+   * @param ctx the `Context` whose body is assumed to contain
+   *  a *single* `Product`.
+   * @return the `Product` extracted from the given `Context`.
+   */
+  private Product returnedSingleProduct(Context ctx) {
+    String result = ctx.resultString();
+    Product product = javalinJackson.fromJsonString(result, Product.class);
+    return product;
+  }
+
+
+  @Test
+  public void canGetAllProducts() {
+
+    String path = "api/products";
+    Context ctx = mockContext(path);
+
+    productController.getProducts(ctx);
+    Product[] returnedProducts = returnedProducts(ctx);
+
+    // The response status should be 200, i.e., our request
+    // was handled successfully (was OK). This is a named constant in
+    // the class HttpCode.
+    assertEquals(HttpCode.OK.getStatus(), mockRes.getStatus());
+    assertEquals(
+      db.getCollection("products").countDocuments(),
+      returnedProducts.length
+    );
+  }
+
+  @Test
+  public void canGetProductWithNameApple() {
+
+    mockReq.setQueryString("name=apple");
+    String path = "api/products";
+    Context ctx = mockContext(path);
+
+    productController.getProducts(ctx);
+    Product[] returnedProducts = returnedProducts(ctx);
+
+    // The response status should be 200, i.e., our request
+    // was handled successfully (was OK). This is a named constant in
+    // the class HttpCode.
+    assertEquals(HttpCode.OK.getStatus(), mockRes.getStatus());
+    assertEquals(1, returnedProducts.length);
+    assertEquals(
+      db.getCollection("products").countDocuments(),
+      returnedProducts.length
+    );
+  }
 
 }
--- server/src/test/java/umm3601/user/UserControllerSpec.java
+++ server/src/test/java/umm3601/user/UserControllerSpec.java
@@ -1,5 +1,49 @@
 package umm3601.user;
 
+import static com.mongodb.client.model.Filters.eq;
+import static io.javalin.plugin.json.JsonMapperKt.JSON_MAPPER_KEY;
+import static java.util.Map.entry;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpServletResponse;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.javalin.core.JavalinConfig;
+import io.javalin.core.validation.ValidationException;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.HandlerType;
+import io.javalin.http.HttpCode;
+import io.javalin.http.NotFoundResponse;
+import io.javalin.http.util.ContextUtil;
+import io.javalin.plugin.json.JavalinJackson;
+
 /**
  * Tests the logic of the UserController
  *
