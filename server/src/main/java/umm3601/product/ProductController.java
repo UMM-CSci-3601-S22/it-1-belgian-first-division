f6569836945a3fabb40c88bd76998b74a2cc87df
Zeke <krug0102@morris.umn.edu>
2022-03-09 15:00:44 -0600

    Add server side functionality to get a product by id

--- server/src/main/java/umm3601/product/ProductController.java
+++ server/src/main/java/umm3601/product/ProductController.java
@@ -2,16 +2,10 @@ package umm3601.product;
 
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
-// import static com.mongodb.client.model.Filters.regex;
 
-// import java.nio.charset.StandardCharsets;
-// import java.security.MessageDigest;
-// import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
-// import java.util.Objects;
 import java.util.Map;
-// import java.util.regex.Pattern;
 
 import com.mongodb.client.MongoDatabase;
 // import com.mongodb.client.model.Sorts;
@@ -20,12 +14,12 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.bson.UuidRepresentation;
 import org.bson.conversions.Bson;
-// import org.bson.types.ObjectId;
+import org.bson.types.ObjectId;
 import org.mongojack.JacksonMongoCollection;
 
-// import io.javalin.http.BadRequestResponse;
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
-// import io.javalin.http.NotFoundResponse;
+import io.javalin.http.NotFoundResponse;
 import io.javalin.http.HttpCode;
 
 public class ProductController {
@@ -41,6 +35,22 @@ public class ProductController {
         "products",
         Product.class,
         UuidRepresentation.STANDARD);
+  }
+
+  public void getProduct(Context ctx) {
+    String id = ctx.pathParam("id");
+    Product product;
+
+    try {
+      product = productCollection.find(eq("_id", new ObjectId(id))).first();
+    } catch (IllegalArgumentException e) {
+      throw new BadRequestResponse("The requested product id wasn't a legal Mongo Object ID.");
+    }
+    if (product == null) {
+      throw new NotFoundResponse("The requested product was not found");
+    } else {
+      ctx.json(product);
+    }
   }
 
   public void getProducts(Context ctx) {
